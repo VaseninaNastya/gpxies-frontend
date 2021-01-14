97ffca1b246096c06e9d0c3ef4a37cc9a8d6cccf
VaseninaNastya <dadasiegt@yandex.ru>
2021-01-14 22:36:33 +0300

    fix: fix userName in header error

--- src/js/Header.js
+++ src/js/Header.js
@@ -5,7 +5,7 @@ class Header {
     this.logout_button = create("a", "logout_button", "Выйти");
     this.logoutButtonAddEventListener()
     const logo = create("div", "logo", "gpXies");
-    const userName = create("div", "header_userName", localStorage.getItem('userName'))
+    const userName = create("div", "header_userName", localStorage.getItem('gpxiesUserName'))
     const header = create("header", null, 
       create("div", "container header_container", [logo,userName,this.logout_button]),
     );
