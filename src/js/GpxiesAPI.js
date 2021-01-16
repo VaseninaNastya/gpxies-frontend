0415b4f9816dc57a3984e8ce43e391c62c2eae95
VaseninaNastya <dadasiegt@yandex.ru>
2021-01-17 02:52:41 +0300

    feat: add upload track data to server

--- src/js/GpxiesAPI.js
+++ src/js/GpxiesAPI.js
@@ -38,5 +38,26 @@ class GpxiesAPI {
       })
       .catch((error) => Error(error));
   }
+  async tracksDataUpload(tracksData) {
+    return fetch(this.API_SERVER + "/tracks/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+        "Referer": "https://api.gpxies.ru",
+        "Authorization": "Bearer "+ localStorage.getItem("gpxiesToken")
+      },
+      body: JSON.stringify(tracksData),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonData)=>{
+        return jsonData;
+      })
+      .catch((error) => Error(error));
+  }
+  async tracksFileUpload(tracksFile) {}
+  
 }
 export default GpxiesAPI;
--- src/js/LoadTrackPage.js
+++ src/js/LoadTrackPage.js
@@ -1,6 +1,8 @@
 import create from "./create";
 import "../css/main.css";
 import Header from "./Header";
+import GpxiesAPI from "./GpxiesAPI";
+import Type from "./trackTypes.utils";
 class LoadTrackPage {
   generateLayout() {
     const loading_button = create(
@@ -17,73 +19,67 @@ class LoadTrackPage {
       null,
       ["type", "file"],
       ["id", "load_track_file_input"],
-      /*["accept", "image/*,image/jpeg"]*/
-      ["accept", ".gpx"]
+      ["accept", ".gpx"],
+      ["name", "file"]
     );
     this.loading_trackFileName = create(
       "div",
       "loading_trackFileName loading_trackFileName__hidden"
     );
-    const button_save = create(
+    this.button_save = create(
       "div",
       "button_save button__primary",
-      "Сохранить"
+      "Сохранить и отправить"
     );
     const header = new Header();
     this.button__prime = create("a", "button__primary", "Сохранить");
-    const private_checkbox_container = create(
+    this.private_checkbox = create(
+      "input",
+      null,
+      null,
+      null,
+      ["type", "checkbox"],
+      ["value", "1"],
+      ["id", "private_checkbox"]
+    );
+    this.private_checkbox_container = create(
       "div",
       "private_checkbox_container",
       [
-        create(
-          "input",
-          null,
-          null,
-          null,
-          ["type", "checkbox"],
-          ["value", "1"],
-          ["id", "private_checkbox"]
-        ),
+        this.private_checkbox,
         create("label", "private_checkbox_label", "Приватный трек", null, [
           "for",
           "private_checkbox",
         ]),
       ]
     );
-
-    const sport_selector = create(
+    this.track_description_textarea = create(
+      "textarea",
+      "track_description_textarea",
+      null,
+      null,
+      ["id", "track_description_textarea"]
+    );
+    this.sport_selector = create(
       "select",
       "sport_selector",
       [
-        create(
-          "option",
-          null,
-          "Велосипед",
-          null,
-          ["value", "1"],
-          ["id", "cycling"],
-          ["selected", "selected"]
-        ),
-        create("option", null, "Бег", null, ["value", "2"], ["id", "runing"]),
-        create(
-          "option",
-          null,
-          "Ходьба",
-          null,
-          ["value", "3"],
-          ["id", "walking"]
-        ),
+        create("option", null, "Велосипед", null, ["value", Type.Bike], ["selected", "selected"]),
+        create("option", null, "Бег", null, ["value", Type.Run]),
+        create("option", null, "Ходьба", null, ["value", Type.Hike]),
       ],
       null,
       ["id", "sport_selector"]
     );
-    const trackName_input = create(
+    this.trackName_input = create(
       "input",
       "loadTrackPage_name",
       null,
       null,
       ["placeholder", "Название трека"],
-      ["type", "text"]
+      ["type", "text"],
+      ["required", "required"],
+      ["id", "loadTrackPage_name"]
     );
     const loadTrackPage_container = create("div", "loadTrackPage_container", [
       create("h2", "loadTrackPage_title", "Загрузить новый трек"),
@@ -93,48 +89,32 @@ class LoadTrackPage {
         this.loading_trackFileName,
       ]),
       create("div", "track_description_container", [
-        sport_selector,
-        trackName_input,
-        private_checkbox_container,
+        this.sport_selector,
+        this.trackName_input,
+        this.private_checkbox_container,
       ]),
       create("div", "track_description_textarea_container", [
         create("label", "track_description_label", "Oписание трека", null, [
           "for",
           "track_description_textarea",
         ]),
-        create(
-          "textarea",
-          "track_description_textarea",
-          null,
-          null,
-          /*["type", "textarea"],*/
-          ["id", "track_description_textarea"]
-        ),
+        this.track_description_textarea,
       ]),
-      button_save,
+      this.button_save,
     ]);
     const wraper = create("div", "loadTrackPage_wrapper", [
       header.generateLayout(),
       loadTrackPage_container,
     ]);
     this.addLoadingButtonEventListener();
+    this.addButtonSaveEventListener();
     document.body.prepend(wraper);
   }
   addLoadingButtonEventListener() {
-    /*<input type="file" onchange="showFile(this)">
-
-<script>
-function showFile(input) {
-  let file = input.files[0];
-
-  alert(`File name: ${file.name}`); // например, my.png
-  alert(`Last modified: ${file.lastModified}`); // например, 1552830408824
-}
-</script>
-*/
     this.loading_hiddenInput.addEventListener(
       "change",
       () => {
+        console.log("this.loading_hiddenInput", this.loading_hiddenInput.value);
         const fileList = this.loading_hiddenInput.files[0];
         const fileName = fileList.name;
         this.loading_trackFileName.append(fileName);
@@ -143,6 +123,21 @@ function showFile(input) {
       },
       false
     );
+  }
+  addButtonSaveEventListener() {
+    this.button_save.addEventListener("click", async (event) => {
+      event.preventDefault();
+
+      let tracksData = {
+        title: this.trackName_input.value,
+        type: this.sport_selector.value,
+        description: this.track_description_textarea.value,
+        isPrivate: this.private_checkbox.checked,
+      };
+      this.gpxiesAPI = new GpxiesAPI();
+      let res = await this.gpxiesAPI.tracksDataUpload(tracksData);
+      console.log("res", res);
+    });
   }
 }
 
