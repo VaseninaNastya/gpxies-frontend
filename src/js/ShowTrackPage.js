130e3bb5b4a229c5cfa9d15f81dfc4c16971d5f5
Nikolai Minkevich <minkevich.nikolai@yandex.ru>
2021-01-18 18:25:58 +0300

    feat: add map

--- src/js/ShowTrackPage.js
+++ src/js/ShowTrackPage.js
@@ -1,16 +1,18 @@
-import  '../css/main.css';
+import '../css/main.css';
 import create from "./create";
 import ShowTrackPageHeader from "./ShowTrackPageHeader";
 import Footer from "./Footer";
 import СompleteStatictics from "./СompleteStatictics";
 import Header from './Header';
+import WorldMap from './WorldMap';
+
 class ShowTrackPage {
   generateLayout() {
     const completeStatictics = new СompleteStatictics();
     const showTrackPageHeader = new ShowTrackPageHeader();
     const header = new Header();
     const footer = new Footer();
-    const map = create("div", "map")
+    const map = create("div", "map", null, null, ["id", "mapid"])
     document.body.prepend(
       create("div", "showTrackPage_wrapper", [
         header.generateLayout(),
@@ -20,6 +22,8 @@ class ShowTrackPage {
         footer.generateLayout(),
       ])
     );
+    this.worldMap = new WorldMap();
+    this.worldMap.generateLayout();
   }
 }
 
