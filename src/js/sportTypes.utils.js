e0f1283073e5977a009b6d8a0585604dd7570a13
VaseninaNastya <dadasiegt@yandex.ru>
2021-01-19 21:36:40 +0300

    feat: add delete button

---
+++ src/js/sportTypes.utils.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Run: '../../assets/img/icon_run.png',
+    Bike: '../../assets/img/icon_bike.png',
+    Hike: '../../assets/img/icon_walk.png',
+    Ski: '../../assets/img/icon_run.png',
+    Kayak: '../../assets/img/icon_run.png',
+    Other: '../../assets/img/icon_run.png'
+  }
