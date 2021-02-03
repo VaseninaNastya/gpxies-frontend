b2cfa7e646bfeda835172e56b884d57315f67773
Nikolai Minkevich <minkevich.nikolai@yandex.ru>
2021-02-04 02:17:00 +0300

    fix: fix filter

--- src/js/SearchBar.js
+++ src/js/SearchBar.js
@@ -17,11 +17,10 @@ class SearchBar {
     addSearchBarEventListener(){
         this.searchBar.addEventListener('input', () => {
             let searchPhrase = document.querySelector('.searchBar_input').value
-            Array.from(document.querySelectorAll('.table_body_container')).map((item) => {
-                console.log("item",item);
+            Array.from(document.querySelectorAll('.table_body_row')).map((item) => {
                 item.classList.remove('hidden_by_search');
             });
-            Array.from(document.querySelectorAll('.table_body_container')).map((item) => {
+            Array.from(document.querySelectorAll('.table_body_row')).map((item) => {
                 if (!item.childNodes[3].childNodes[0].textContent.toLowerCase().includes(searchPhrase.toLowerCase())) {
                     item.classList.add('hidden_by_search');
                 }
