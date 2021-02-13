4e5c41b5bc3ad99adb6417734d6017d54ee4173b
Nikolai Minkevich <minkevich.nikolai@yandex.ru>
2021-02-14 00:25:39 +0300

    feat: add custom markers

--- src/js/WorldMap.js
+++ src/js/WorldMap.js
@@ -11,6 +11,20 @@ class WorldMap {
     this.MODE_EDIT = "edit";
     this.MODE_VIEW = "view";
     this.MODE_CREATE = "create";
+    ///
+
+    this.blueIcon = L.icon({
+      iconUrl: "/img/leaflet/button_blue.png",
+      iconSize: [16, 16], // size of the icon
+      iconAnchor: [8, 8], // point of the icon which will correspond to marker's location
+      popupAnchor: [32, 32], // point from which the popup should open relative to the iconAnchor
+    });
+    this.redIcon = L.icon({
+      iconUrl: "/img/leaflet/button_red.png",
+      iconSize: [16, 16], // size of the icon
+      iconAnchor: [8, 8], // point of the icon which will correspond to marker's location
+      popupAnchor: [32, 32], // point from which the popup should open relative to the iconAnchor
+    });
   }
 
   setMode(mode = "view") {
@@ -57,7 +71,7 @@ class WorldMap {
         // Add remove all layers
         //this.clearMapMarkers(this.mymap);
         if (this.mymap.getZoom() >= this.ZOOM_FOR_EDIT) {
-          console.log("Bounds:", this.mymap.getBounds());
+          // console.log("Bounds:", this.mymap.getBounds());
           let mapBounds = this.mymap.getBounds();
 
           // Add increasing of bounds for better smoothy
@@ -66,7 +80,8 @@ class WorldMap {
           mapBounds._northEast.lng += INCREASE_COEFFICIENT;
           mapBounds._southWest.lat -= INCREASE_COEFFICIENT;
           mapBounds._southWest.lng -= INCREASE_COEFFICIENT;
-          console.log("mapBounds", mapBounds);
+
+          // console.log("mapBounds", mapBounds);
           this.markers = [];
           this.polylineCoordinates.map((coord, index) => {
             if (mapBounds.contains(coord)) {
@@ -137,22 +152,30 @@ class WorldMap {
     let self = this;
 
     let marker = L.marker([lat, lon], {
+      icon: this.blueIcon,
       draggable: "true",
-    }).on("dragend", (event) => {
-      var marker = event.target;
-      var position = marker.getLatLng();
-      marker.setLatLng(new L.LatLng(position.lat, position.lng), { draggable: "true" });
-      self.polylineCoordinates[indexOfPolylinePoint] = [position.lat, position.lng];
-      // self.clearMap(self.mymap);
-      // event.target.removeLayer()
-      console.log(self.mymap._layers);
-      this.tracksGroup.clearLayers();
-      const polyline = L.polyline(this.polylineCoordinates, { color: "red" });
+    })
+      .on("dragend", (event) => {
+        var marker = event.target;
+        var position = marker.getLatLng();
+        marker.setLatLng(new L.LatLng(position.lat, position.lng), { draggable: "true" });
+        self.polylineCoordinates[indexOfPolylinePoint] = [position.lat, position.lng];
+        // self.clearMap(self.mymap);
+        // event.target.removeLayer()
+        console.log(self.mymap._layers);
+        this.tracksGroup.clearLayers();
+        const polyline = L.polyline(this.polylineCoordinates, { color: "red" });
 
-      this.tracksGroup.addLayer(polyline).addTo(this.mymap);
-      // L.polyline(self.polylineCoordinates, { color: "red" }).addTo(self.mymap);
-      // self.mymap.panTo(new L.LatLng(position.lat, position.lng));
-    });
+        this.tracksGroup.addLayer(polyline).addTo(this.mymap);
+        // L.polyline(self.polylineCoordinates, { color: "red" }).addTo(self.mymap);
+        // self.mymap.panTo(new L.LatLng(position.lat, position.lng));
+      })
+      .on("mouseover", (event) => {
+        event.target.setIcon(this.redIcon);
+      })
+      .on("mouseout", (event) => {
+        event.target.setIcon(this.blueIcon);
+      });
 
     return marker;
     //this.markerLayer.push(marker);
